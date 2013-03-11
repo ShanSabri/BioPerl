ca66e84e17ca87707c319baa2a394a4470d31359
Shan Sabri <debian>
2013-03-11 18:49:04 -0400

    DONE

--- week_07/q1.pl
+++ week_07/q1.pl
@@ -24,50 +24,62 @@ if(!defined $opt_o && !defined $opt_t)
 }
  
 if(defined $opt_o){
-        print "Searching organism field for $opt_o \n";
+        #print "-o arg is $opt_o\n";
         my $sth = $dbh->prepare
 			("
 			SELECT g.name, o.name, expression_level
                         FROM gene as g, organism as o,
 			     gene_organism_expression as goe,
 			     gene_organism as go
-                        WHERE o.name = 'Mus musculus' 
+                        WHERE o.name = '$opt_o' 
 	                     AND g.id = go.gene_id
 			     AND o.id = go.organism_id 
 			     AND goe.id = go.id") 
-			or die qq("Can't prepare $opt_o");       
+			or die qq("Can't prepare search for $opt_o\n");       
         $sth->execute()
-			or die qq("Can't execute $opt_o");
-
-	if ($sth->rows < 0) {
-		print "NO RESULTS FOR $opt_o\n";
-	}else{
-		print "RESULTS:\nGene\t Organism\t Expression Level\n";
-		while((my($gene, $organism, $gene_organism_expression)) 
-		  = $sth->fetchrow_array()){
-			print "$gene\t $organism\t $gene_organism_expression \n";
+			or die qq("Can't execute search for $opt_o\n");
+	
+	my $results = undef;
+	while ((my($gene, $organism, $gene_organism_expression))
+           = $sth->fetchrow_array()){
+		if (!$results){
+			print "RESULTS:\n\tGene\t Organism\t Expression Level\n";
+			$results = 1;
 		}
-
-	# STUFF BELOW ALSO WORKS!
-	#	while(my @row = $sth->fetchrow_array){
-	#		print "\t@row\n";	
-	#	}	
-	}		
+		print "\t$gene\t $organism\t $gene_organism_expression \n";
+	}
+	print "* NOT FOUND *\n" if !$results;
 }
 
 
-#if($opt_t){
-#        print "optT is $opt_t \n";
-#        my $sth = $dbh->prepare("
-#                        SELECT
-#                        FROM
-#                        WHERE
-#                        ");
-#        $sth->execute();
-#}
+if(defined $opt_t){
+        #print "-t arg is $opt_t\n";
+        my $sth = $dbh->prepare("
+                        SELECT g.name, o.name, expression_level
+                        FROM gene as g, organism as o,
+                             gene_organism_expression as goe,
+                             gene_organism as go,
+			     tissue as t
+                        WHERE t.name = '$opt_t'
+                             AND g.id = go.gene_id
+                             AND o.id = go.organism_id
+                             AND goe.id = go.id
+			     AND t.id = goe.tissue_id")
+                        or die qq("Can't prepare search for $opt_t");
+        $sth->execute()
+                        or die qq("Can't execute search for $opt_t");
+
+	my $results = undef;
+        while ((my($gene, $organism, $gene_organism_expression))
+           = $sth->fetchrow_array()){
+                if (!$results){
+                        print "RESULTS:\n\tGene\t Organism\t Expression Level\n";
+                        $results = 1;
+                }
+                print "\t$gene\t $organism\t $gene_organism_expression \n";
+        }
+        print "* NOT FOUND *\n" if !$results;
+}
+
  
 $dbh->disconnect();
-
-
-#HELP
-#http://www.felixgers.de/teaching/perl/perl_DBI.html
